6a406316ce9dbd7893a0476891b0713356bca14d
Javier <profejaviersalinas@gmail.com>
2025-01-12 19:40:22 -0300

    primer commit

---
+++ Nortur/Nortur/Nortur/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nortur.Startup))]
+namespace Nortur
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
