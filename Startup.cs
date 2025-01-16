2de0fe1cbefad4910e5d9c6d26b921b121c88907
JavierDev <140284597+BlueDesi@users.noreply.github.com>
2025-01-16 20:17:36 -0300

    Add files via upload
    •	RESERVA Y GESTION DE PASAJES De (primer servicio) 07:00 a 19:00 (último servicio)

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Micros.Startup))]
+namespace Micros
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
